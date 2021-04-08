4e6ca2b94ea356a1a3d4f679e01d118bc2f73947
Daniel Tsvirkun <danila.tsvirkun555@gmail.com>
2021-04-08 05:10:36 +0300

    fixed date regex

--- src/transaction/transaction.router.ts
+++ src/transaction/transaction.router.ts
@@ -21,7 +21,7 @@ const addIncomeSchema = Joi.object({
   amount: Joi.number().required().min(1),
   date: Joi.string()
     .custom((value, helpers) => {
-      const dateRegex = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/;
+      const dateRegex = /[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])/;
       const isValidDate = dateRegex.test(value);
       if (!isValidDate) {
         return helpers.message({
@@ -41,7 +41,7 @@ const addExpenseSchema = Joi.object({
   amount: Joi.number().required().min(1),
   date: Joi.string()
     .custom((value, helpers) => {
-      const dateRegex = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/;
+      const dateRegex = /[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])/;
       const isValidDate = dateRegex.test(value);
       if (!isValidDate) {
         return helpers.message({
